18de098c76f323a1c6f4d8ada7ebda9ba6ce2860
guzusoftware <guzusoftware@gmail.com>
2026-08-06 18:55:42 +0200

    Pass Keystatic API configuration

--- src/pages/api/keystatic/index.ts
+++ src/pages/api/keystatic/index.ts
@@ -1,4 +1,4 @@
 import { makeHandler } from '@keystatic/astro/api'
 import config from '../../../../keystatic.config'
 
-export const ALL = makeHandler(config)
+export const ALL = makeHandler({ config })
